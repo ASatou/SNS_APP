85701e7e6a81d054ed038539410e3d23ffb4e5b0
Ubuntu <ubuntu@ip-172-31-45-64.ap-northeast-1.compute.internal>
2020-05-12 09:49:05 +0000

    Generated users controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  
+  root 'users#login'
+  
+  get 'users/login'
+  get 'users/new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
