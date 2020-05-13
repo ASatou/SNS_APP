688b028cbd0477e4297d3e4a50769065cba4d977
Ubuntu <ubuntu@ip-172-31-45-64.ap-northeast-1.compute.internal>
2020-05-13 07:45:57 +0000

    Modeled user and Add name/email validation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,24 @@
 class UsersController < ApplicationController
+  
   def login
+    @user = User.find_by(
+      email: params[:email]
+      )
   end
 
   def new
+    @user = User.new(
+      name: params[:name],
+      email: params[:email]
+      )
+      
+    if @user.save
+      #Success
+    else
+      #Failure
+      flash[:notice] = "入力内容に誤りがあります"
+      # render signup_path
+    end
+    
   end
 end
