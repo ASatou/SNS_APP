5acb89e9726550bf1ebc050bdb0176c1fe44f0f0
Ubuntu <ubuntu@ip-172-31-45-64.ap-northeast-1.compute.internal>
2020-05-20 14:01:37 +0000

    Added Remember_token

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_14_003608) do
+ActiveRecord::Schema.define(version: 2020_05_20_092306) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2020_05_14_003608) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "password_digest"
     t.string "full_name"
+    t.string "remember_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
