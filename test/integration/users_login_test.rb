065fa290444eb95e32b967e99a4cd78b84fccddc
Ubuntu <ubuntu@ip-172-31-45-64.ap-northeast-1.compute.internal>
2020-05-21 09:30:49 +0000

    Fixed some bugs and Added test of remember_me

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -17,6 +17,9 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert_template 'users/show'
     assert_select "a[href=?]", logout_path
     assert_select "a[href=?]", user_path(@user)
+    delete logout_path
+    follow_redirect!
+    assert_template 'sessions/new'
   end
   
   test "login with invalid information" do
@@ -28,5 +31,22 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     get root_path
     assert flash.empty?
   end
+  
+  test "authenticated? should return false when user's remember_digest is nil" do
+    assert_not @user.authenticated?('')
+  end
+  
+  test "login with remembering" do
+    log_in_as(@user, remember_me: '1')
+    assert_not_empty cookies[:remember_token]
+  end
+  
+  test "login without remembering" do
+    log_in_as(@user, remember_me: '1')
+    delete logout_path
+    log_in_as(@user, remember_me: '0')
+    assert_empty cookies[:remember_token]
+  end
+  
   
 end
