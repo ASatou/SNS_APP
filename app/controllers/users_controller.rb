2bfa5c9a5bd95e3c674616d29a7fca5c9685a6f2
Ubuntu <ubuntu@ip-172-31-45-64.ap-northeast-1.compute.internal>
2020-05-24 10:51:34 +0000

    Added users index page and Added login authorization

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,10 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:index, :edit, :update]
+  before_action :correct_user, only: [:edit, :update]
+  
+  def index
+    @user = User.all
+  end
   
   def new
     @user = User.new
@@ -39,5 +45,18 @@ class UsersController < ApplicationController
   private
     def user_params
       params.require(:user).permit(:full_name, :name, :email, :password, :password_confirmation)
+    end
+    
+    def logged_in_user
+      unless logged_in?
+        store_location
+        flash[:danger] = "ログインしてください"
+        redirect_to login_url
+      end
+    end
+    
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to root_url unless current_user?(@user)
     end
 end
