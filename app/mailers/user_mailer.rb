5ddf14a033fea0aa7db18ee5789ef8dac553689a
Ubuntu <ubuntu@ip-172-31-45-64.ap-northeast-1.compute.internal>
2020-05-26 23:08:42 +0000

    Added password_reset mailer and password_reset test

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -6,9 +6,8 @@ class UserMailer < ApplicationMailer
     mail to: user.email, subject: "Sample Appのアカウント認証"
   end
 
-  def password_reset
-    @greeting = "Hi"
-
-    mail to: "to@example.org"
+  def password_reset(user)
+    @user = user
+    mail to: user.email, subject: "パスワード再設定用URLの送付"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   
   
+  get 'password_resets/new'
+  get 'password_resets/edit'
   root 'sessions#new'
   
   get '/signup', to: 'users#new'
@@ -12,5 +14,6 @@ Rails.application.routes.draw do
   
   resources :users
   resources :account_activations, only: [:edit]
+  resources :password_resets, only: [:new, :create, :edit, :update]
   
 end
