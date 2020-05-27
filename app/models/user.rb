3a455de23bc143f0f2c234b168285f9e79fc1149
Ubuntu <ubuntu@ip-172-31-45-64.ap-northeast-1.compute.internal>
2020-05-27 09:49:03 +0000

    Added edit/update action in PasswordResetsController

--- app/models/user.rb
+++ app/models/user.rb
@@ -47,11 +47,16 @@ class User < ApplicationRecord
   
   def create_reset_digest
     self.reset_token = User.new_token
-    update_attribute(reset_digest: User.digest(:reset_token), rese_sent_at: Time.zone.now)
+    update_attribute(:reset_digest, User.digest(reset_token))
+    update_attribute(:reset_sent_at, Time.zone.now)
   end
   
   def send_password_reset_email
     UserMailer.password_reset(self).deliver_now
+  end
+  
+  def password_reset_expired?
+    reset_sent_at < 2.hours.ago
   end
 
   private
