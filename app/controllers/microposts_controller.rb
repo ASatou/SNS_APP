dec99c4d8681bf8be76c556030a0ff4bd7177be2
Ubuntu <ubuntu@ip-172-31-45-64.ap-northeast-1.compute.internal>
2020-05-31 01:41:30 +0000

    Added feed_items viewer and test

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,2 +1,43 @@
 class MicropostsController < ApplicationController
+  before_action :logged_in_user, only: [:neew, :index, :create, :destroy]
+  before_action :correct_user, only: [:destroy]
+  
+  def new
+    @micropost = current_user.microposts.build
+    @feed_items = current_user.feed.paginate(page: params[:page])
+  end
+  
+  def index
+    @micropost = current_user.microposts.build
+    @feed_items = current_user.feed.paginate(page: params[:page])
+  end
+  
+  def create
+    @micropost = current_user.microposts.build(micropost_params)
+    if @micropost.save
+      flash[:success] = "投稿が完了しました"
+      redirect_to microposts_path
+    else
+      @feed_items = []
+      render 'new'
+    end
+  end
+  
+  def destroy
+    @micropost.destroy
+    flash[:success] = "投稿が削除されました"
+    redirect_to request.referrer || microposts_url
+  end
+  
+  private
+    
+    def micropost_params
+      params.require(:micropost).permit(:content)
+    end
+    
+    def correct_user
+      @micropost = current_user.microposts.find_by(id: params[:id])
+      redirect_to microposts_path if @micropost.nil?
+    end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -12,8 +12,11 @@ Rails.application.routes.draw do
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
   
+  get '/microposts', to: 'microposts#index'
+  
   resources :users
   resources :account_activations, only: [:edit]
   resources :password_resets, only: [:new, :create, :edit, :update]
+  resources :microposts, only: [:new, :create, :destroy]
   
 end
